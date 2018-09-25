7b819b0eee176edc0bb03b7b02abc73509b80824
张哲铭 <1371955569@qq.com>
2018-09-25 14:13:27 +0800

    hello world

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpRespose
 
-# Create your views here.
+
+def index(request):
+    return HttpRespose("Hello,world. You're at the polls index.")
